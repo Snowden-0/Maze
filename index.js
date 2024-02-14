1b3ac1b78360a9bda46d6f7475c3095ad0892c82
Snowden-0 <uurrehman735@gmail.com>
2024-02-14 08:51:39 +0500

    14 x 8 maze

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 4;
-const cellsVertical = 4;
+const cellsHorizontal = 14;
+const cellsVertical = 8;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
